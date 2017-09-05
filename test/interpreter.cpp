e2e1d4f90508773d0798d80aecc7830e2f92bcab
Josef Kolář <thejoeejoee@gmail.com>
2017-09-05 15:34:23 +0200

    Add sample test fixture

--- test/interpreter.cpp
+++ test/interpreter.cpp
@@ -2,12 +2,24 @@
 
 #include "../src/interpreter.c"
 
-TEST(Interpreter, multiplied_answer_to_everything) {
+class SampleTestFixture : public ::testing::Test {
+protected:
+    int foo;
+    int bar;
+
+    SampleTestFixture() {
+        foo = 42;
+        bar = 5 * foo;
+    };
+};
+
+TEST_F(SampleTestFixture, multiplied_answer_to_everything) {
     EXPECT_EQ(
             return_multiplied_answer_to_everything(5),
-            5 * 42
+            bar
     );
 }
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);

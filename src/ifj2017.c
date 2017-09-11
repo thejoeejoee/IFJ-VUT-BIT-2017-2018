ae898ce633e49e8ec5d2f36974442695f931756d
Josef Kolář <thejoeejoee@gmail.com>
2017-09-11 11:39:17 +0200

    Remove dummy debug structure

--- src/ifj2017.c
+++ src/ifj2017.c
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 #include "interpreter.h"
 
-typedef struct debug_structure_t {
-    int key;
-    struct debug_structure_t * next;
-} DebugStructure;
-
 int main(int argc, char *argv[]) {
-    DebugStructure a, b, c;
-    a.key = 42;
-    a.next = &b;
-    b.next = &c;
-    b.key = 38;
-    c.next = NULL;
-    c.key = 60;
-
     printf("Hello IFJ %d!\n", return_multiplied_answer_to_everything(1));
     return EXIT_SUCCESS;
 }

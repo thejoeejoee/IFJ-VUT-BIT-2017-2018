194d68609e8ed73f4c62b2b66a32c580eeb668fc
Josef Kolář <thejoeejoee@gmail.com>
2017-09-19 01:01:36 +0200

    Clean of plot.ly upload script

--- utils/plotly/plotly_upload_results.py
+++ utils/plotly/plotly_upload_results.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
+# coding=utf-8
 
+import json
 import os
 import sys
-import json
+from operator import itemgetter
+
 import plotly
-import plotly.plotly as py
 import plotly.graph_objs as go
+import plotly.plotly as py
+from plotly.exceptions import PlotlyError
+
+PLOTLY_FILENAME = "IFJ_benchmark"
 
 
-# TODO: refactor needed
-def parse(s):
+def parse_numeric(s):
     try:
         return int(s)
     except ValueError:
@@ -19,42 +24,34 @@ def parse(s):
 def conv(v):
     if isinstance(v, list):
         return v
-    elif isinstance(v, int) or isinstance(v, float):
+    elif isinstance(v, (int, float)):
         return [v]
     elif isinstance(v, str):
-        return [parse(v)]
+        return [parse_numeric(v)]
 
 
 def plotly_combine(data_a, data_b):
     result = []
-    # get set of names
-    names = []
-    for data in data_a:
-        names += [data["name"]]
-    for data in data_b:
-        names += [data["name"]]
-    names = set(names)
+    names = set(tuple(map(itemgetter('name'), data_a)) + tuple(map(itemgetter('name'), data_b)))
 
     # combine data
     for name in names:
         xy_tuples = []
-        tmp_data = {"type": "scatter",
-                    "name": name,  # name
-                    "hoverinfo": "x+y+name",
-                    "x": [],  # build number
-                    "y": []  # time
-                    }
-        for data in data_a:
-            if "name" in data.keys() and data["name"] == name:
-                xy_tuples.extend(zip(conv(data["x"]), conv(data["y"])))
-        for data in data_b:
-            if "name" in data.keys() and data["name"] == name:
-                xy_tuples.extend(zip(conv(data["x"]), conv(data["y"])))
+        tmp_data = {
+            "type": "scatter",
+            "name": name,  # name
+            "hoverinfo": "x+y+name",
+            "x": [],  # build number
+            "y": []  # time
+        }
+        for item in tuple(data_a) + tuple(data_b):
+            if "name" in item.keys() and item["name"] == name:
+                xy_tuples.extend(zip(conv(item["x"]), conv(item["y"])))
 
         unified_tuples = list(dict(xy_tuples).items())
-        unified_tuples.sort(key=lambda tup: tup[0])
+        unified_tuples.sort(key=itemgetter(0))
 
-        if (unified_tuples):
+        if unified_tuples:
             tmp_data["x"], tmp_data["y"] = zip(*unified_tuples)
             tmp_data["x"] = list(tmp_data["x"])
             tmp_data["y"] = list(tmp_data["y"])
@@ -64,79 +61,95 @@ def plotly_combine(data_a, data_b):
     return result
 
 
-# init
-if len(sys.argv) == 3:
-    build_nr = sys.argv[1]
-    filename = sys.argv[2]
-else:
-    print("usage: USER=user API_KEY=key plotly_upload_results.py BUILD_NUMBER benchmark_results.json")
-    exit()
+def get_upload_metadata():
+    if len(sys.argv) == 3:
+        return sys.argv[1], sys.argv[2]
 
-# get benchmark results
-try:
-    with open(filename, "r") as data_file:
-        benchmark_data = json.load(data_file)
-except FileExistsError:
-    print("Warning: benchmark file \"{0}\" not found".format(filename))
-    exit()
+    print(
+        "Wrong script run, usage: USER=user API_KEY=key plotly_upload_results.py BUILD_NUMBER benchmark_results.json"
+    )
+    return None, None
 
-new_data = []
-for benchmark in benchmark_data["benchmarks"]:
-    tmp_data = {"name": benchmark["name"],
-                "x": build_nr,
-                "y": benchmark["time_ms"]
-                }
-    new_data += [tmp_data]
 
-# login
-usr = os.environ.get('USER')
-key = os.environ.get('API_KEY')
-if usr is None or key is None:
-    print("Environment variables USER or API_KEY are missing!")
-    exit()
+def get_new_benchmark_data(filename):
+    # get benchmark results
+    with open(filename) as data_file:
+        return json.load(data_file)
 
-try:
+
+def get_transformed_benchmark_data(filename, build_nr):
+    try:
+        benchmark_data = get_new_benchmark_data(filename)
+    except (FileNotFoundError, ValueError):
+        print('Benchmark data file not found or is not valid JSON.')
+        return []
+
+    return [
+        dict(
+            name=benchmark["name"],
+            x=build_nr,
+            y=benchmark["time_ms"]
+        ) for benchmark in benchmark_data['benchmarks']
+    ]
+
+
+def try_to_sign_in():
+    usr = os.environ.get('USER')
+    key = os.environ.get('API_KEY')
+    if usr is None or key is None:
+        print("Environment variables USER or API_KEY are missing!")
     py.sign_in(usr, key)
 
-    # get plot_url or create new plot
-    dummy_fig = {'data': [{'type': 'scatter', 'x': [], 'y': []}]}
-    plot_url = py.plot(dummy_fig, filename="IFJ_benchmark", fileopt='append', auto_open=False)
 
-    # get plotly figure
-    fig = py.get_figure(plot_url)
+def send_data(new_data):
+    try:
+        # get plot_url or create new plot
+        dummy_fig = {'data': [{'type': 'scatter', 'x': [], 'y': []}]}
+        plot_url = py.plot(dummy_fig, filename=PLOTLY_FILENAME, fileopt='append', auto_open=False)
 
-    # reset figure layout
-    layout = go.Layout(
-        title="IFJ Benchmark",
-        titlefont=dict(size=20),
-        xaxis=dict(
-            title='<b>build #</b>',
-            tickprefix='#',
+        # get plotly figure
+        fig = py.get_figure(plot_url)
+
+        # reset figure layout
+        layout = go.Layout(
+            title="IFJ Benchmark",
             titlefont=dict(size=20),
-            type='category',
-            categoryorder='array',
-            showticklabels=True,
-            autorange=True
-        ),
-        yaxis=dict(
-            title='time [ms]',
-            ticksuffix='ms',
-            titlefont=dict(size=20),
-            showticklabels=True,
-            autorange=True
+            xaxis=dict(
+                title='<b>build #</b>',
+                tickprefix='#',
+                titlefont=dict(size=20),
+                type='category',
+                categoryorder='array',
+                showticklabels=True,
+                autorange=True
+            ),
+            yaxis=dict(
+                title='time [ms]',
+                ticksuffix='ms',
+                titlefont=dict(size=20),
+                showticklabels=True,
+                autorange=True
+            )
         )
-    )
 
-    # combine with new data
-    fig["data"] = plotly_combine(fig["data"], new_data)
-    fig["layout"] = layout
+        # combine with new data
+        fig["data"] = plotly_combine(fig["data"], new_data)
+        fig["layout"] = layout
 
-    # upload
+        return py.plot(fig, filename=PLOTLY_FILENAME, fileopt="overwrite", auto_open=False)
 
-    result_url = py.plot(fig, filename="IFJ_benchmark", fileopt="overwrite", auto_open=False)
-    print("Benchmark results upload complete. ({0})".format(result_url))
-    exit()
+    except Exception as e:
+        print("Warning: benchmark results upload failed: ", e)
 
-except:
-    print("Warning: benchmark results upload failed")
-    exit()
+
+# ini
+build_nr, benchmark_filename = get_upload_metadata()
+data = get_transformed_benchmark_data(benchmark_filename, build_nr)  # login
+
+try:
+    try_to_sign_in()
+except PlotlyError as e:
+    print('Failed to sign in: ', e)
+else:
+    url = send_data(data)
+    print("Benchmark results upload complete. ({0})".format(url))

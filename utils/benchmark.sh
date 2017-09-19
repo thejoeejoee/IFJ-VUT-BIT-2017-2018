64c0abeda6f694b804b3f57b20ac4b5b525070c3
Josef Kolář <thejoeejoee@gmail.com>
2017-09-19 17:38:21 +0200

    Add plotly deps to travis build

--- utils/benchmark.sh
+++ utils/benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "${CC}" == "gcc" ]; then
-    ${TRAVIS_BUILD_DIR}/ifj2017_benchmark --benchmark_format=json > ${TRAVIS_BUILD_DIR}/benchmark_results.json;
-    USER=${PLOTLY_USER} API_KEY=${PLOTLY_API_KEY} ${TRAVIS_BUILD_DIR}/utils/plotly/plotly_upload_results.py ${TRAVIS_BUILD_NUMBER} ${TRAVIS_BUILD_DIR}/benchmark_results.json
+    ${TRAVIS_BUILD_DIR}/build/ifj2017_benchmark --benchmark_format=json > ${TRAVIS_BUILD_DIR}/benchmark_results.json;
+    USER=${PLOTLY_USER} API_KEY=${PLOTLY_API_KEY} python3 ${TRAVIS_BUILD_DIR}/utils/plotly/plotly_upload_results.py ${TRAVIS_BUILD_NUMBER} ${TRAVIS_BUILD_DIR}/benchmark_results.json
 fi

5013686662f3b8f5b85ab1cda4ac9dfa1ba0fec8
Martin Kobelka <martinkobelka@gmail.com>
2017-09-22 10:55:40 +0200

    Repair error in get token, add identifier token and states

---
+++ test/tokenizer.cpp
@@ -0,0 +1,84 @@
+#include <utility>
+
+#include "gtest/gtest.h"
+#include "utils/stringbycharprovider.h"
+
+
+extern "C" {
+#include "../src/lexer.h"
+#include "../src/memory.h"
+}
+
+class LexerTokenizerTestFixture : public ::testing::Test {
+    protected:
+        Lexer *lexer;
+
+        virtual void SetUp() {
+            memory_manager_enter(nullptr);
+            lexer = lexer_init(token_stream);
+        }
+
+};
+
+
+
+TEST_F(LexerTokenizerTestFixture, MathTokens) {
+    StringByCharProvider* provider = StringByCharProvider::instance();
+    char_stack_free(&(lexer->stack));
+
+    provider->setString("+ \n -     + \t *");
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_ADD
+    ) << "Error get add token";
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_SUBTRACT
+    ) << "Error get subtract token";
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_ADD
+    ) << "Error get add token";
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_MULTIPLY
+    ) << "Error get multiply token";
+}
+
+
+TEST_F(LexerTokenizerTestFixture, RelationOperators) {
+    StringByCharProvider* provider = StringByCharProvider::instance();
+    char_stack_free(&(lexer->stack));
+
+    provider->setString("<= >= < >");
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_SMALLER_SAME
+    ) << "Error get SMALLER_SAME token";
+}
+
+TEST_F(LexerTokenizerTestFixture, Identifiers) {
+    StringByCharProvider* provider = StringByCharProvider::instance();
+    char_stack_free(&(lexer->stack));
+
+    provider->setString("aaaaab A8_B _888AZa");
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_IDENTIFIER
+
+    ) << "Error get IDENTIFIER token";
+
+    EXPECT_EQ(
+            lexer_next_token(lexer)->type,
+            TOKEN_IDENTIFIER
+
+    ) << "Error get IDENTIFIER token";
+}
+
+

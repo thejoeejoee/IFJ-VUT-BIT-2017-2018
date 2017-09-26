582cf14170c79f2ff253adb54afe473143fb481d
Tomáš Pazdiora <tomas.pazdiora@gmail.com>
2017-09-26 17:29:05 +0200

    Add reference benchmark

---
+++ benchmark/reference_benchmark.cpp
@@ -0,0 +1,21 @@
+#include <benchmark/benchmark.h>
+
+static void ref() {
+	const int cnt = 1000;
+	for (int i = 0; i < cnt; i++) {
+#ifdef _MSC_VER
+		__asm { nop };
+#else 
+		__asm__ __volatile__("nop");
+#endif
+	}
+}
+
+static void ReferenceBenchmark(benchmark::State& state) {
+	while (state.KeepRunning()) {
+		ref();
+	}
+}
+
+// Register the function as a benchmark
+BENCHMARK(ReferenceBenchmark);

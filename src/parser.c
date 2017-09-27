7082169654c5b3d334ab0585f13058f10c30fbe3
Martin Kobelka <martinkobelka@gmail.com>
2017-09-27 18:05:29 +0200

    Add macros

--- src/parser.c
+++ src/parser.c
@@ -1,13 +1,15 @@
-//
-// Created by martin on 26.9.17.
-//
-
 #include "parser.h"
 #include "lexer.h"
 #include "lexer_fsm.h"
 #include "token.h"
 
-Parser* parser_init(lexer_input_stream_f input_stream) {
+
+#define GET_NEXT_TOKEN_TYPE() token = lexer_next_token(parser->lexer); token_type = token->type; memory_free(token); ;
+
+#define INIT_LOCAL_TOKEN_VARS() Token *token; TokenType token_type; ;
+
+
+Parser *parser_init(lexer_input_stream_f input_stream) {
     Parser* parser = (Parser*) memory_alloc(sizeof(Parser));
 
     NULL_POINTER_CHECK(parser, NULL);
@@ -16,39 +18,35 @@ Parser* parser_init(lexer_input_stream_f input_stream) {
     return parser;
 }
 
-void parser_free(Parser** parser) {
+void parser_free(Parser **parser) {
 
     lexer_free(&((*parser)->lexer));
     memory_free(*parser);
     *parser = NULL;
 }
 
-bool parser_parse_program(Parser* parser) {
-    Token* token;
-    TokenType token_type;
+bool parser_parse_program(Parser *parser) {
 
+    INIT_LOCAL_TOKEN_VARS();
     /*
      * RULE
      * <prog> -> <body> EOF
      */
 
     // Call rule <body>. If <body> return false => return false
-    if(!parser_parse_body(parser))
+    if (!parser_parse_body(parser))
         return false;
 
-    // Expect EOF token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
-    memory_free(token);
+    GET_NEXT_TOKEN_TYPE();
 
-    // If return true, program is syntactically correct
+    // Expect EOF token If return true, program is syntactically correct
     return (token_type == TOKEN_EOF);
 
 }
 
-bool parser_parse_body(Parser* parser) {
-    Token* token;
-    TokenType token_type;
+bool parser_parse_body(Parser *parser) {
+
+    INIT_LOCAL_TOKEN_VARS()
 
     /**
      * RULE
@@ -60,10 +58,8 @@ bool parser_parse_body(Parser* parser) {
         return false;
 
     // Expect SCOPE token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
-    memory_free(token);
-    if(token_type != TOKEN_SCOPE)
+    GET_NEXT_TOKEN_TYPE();
+    if (token_type != TOKEN_SCOPE)
         return false;
 
     // Call <statements> rule
@@ -71,32 +67,30 @@ bool parser_parse_body(Parser* parser) {
         return false;
 
     // Expect END token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
+    GET_NEXT_TOKEN_TYPE();
     if(token_type != TOKEN_END)
         return false;
 
     // Expect SCOPE token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
+    GET_NEXT_TOKEN_TYPE();
     return (token_type == TOKEN_SCOPE);
 }
 
-bool parser_parse_definitions(Parser* parser) {
+bool parser_parse_definitions(Parser *parser) {
 
     // Todo: It is epsilon, it will be implemented in the future
     return true;
 }
 
-bool parser_parse_statements(Parser* parser) {
+bool parser_parse_statements(Parser *parser) {
 
     // Todo: It is epsilon, it will be implemented in the future
     return true;
 }
 
-bool parser_function_param(Parser* parser) {
-    Token* token;
-    TokenType token_type;
+bool parser_function_param(Parser *parser) {
+
+    INIT_LOCAL_TOKEN_VARS()
 
     /**
      * RULE
@@ -104,23 +98,19 @@ bool parser_function_param(Parser* parser) {
      */
 
     // Expect IDENTIFIER token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
-    memory_free(token);
-    if(token_type != TOKEN_IDENTIFIER)
+    GET_NEXT_TOKEN_TYPE();
+    if (token_type != TOKEN_IDENTIFIER)
         return false;
 
     // Expect AS token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
-    memory_free(token);
-    if(token_type != TOKEN_AS)
+    GET_NEXT_TOKEN_TYPE();
+    if (token_type != TOKEN_AS)
         return false;
 
     // Expect TYPE token
-    token = lexer_next_token(parser->lexer);
-    token_type = token->type;
-    memory_free(token);
+    GET_NEXT_TOKEN_TYPE();
     return (token_type == TOKEN_INTEGER || token_type == TOKEN_STRING || token_type == TOKEN_DOUBLE);
 
 }
+
+

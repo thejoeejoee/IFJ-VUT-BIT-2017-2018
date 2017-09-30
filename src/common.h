bde7fcd373e0050499ffa793ba5a170e0e8e941c
Josef Kolář <thejoeejoee@gmail.com>
2017-09-30 12:10:52 +0200

    Add Makefile with packing and deps auto generating

--- src/common.h
+++ src/common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#define UNUSED(x) (void) (x)
 #define MSVC_EXPAND(x) x
 #define GET_FIRST_ARG(N, ...) N
 #define GET_OVERLOADED_MACRO12(_1, _2, ...) MSVC_EXPAND(GET_FIRST_ARG(__VA_ARGS__, 0))
--- src/ifj2017.c
+++ src/ifj2017.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "debug.h"
+#include "common.h"
 
 short log_verbosity = LOG_VERBOSITY_DEBUG;
 
 int main(int argc, char* argv[]) {
-    printf("Hello IFJ!\n");
+    UNUSED(argc);
+    UNUSED(argv);
+
+    printf("Hello world, IFJ!\n");
     return EXIT_SUCCESS;
 }

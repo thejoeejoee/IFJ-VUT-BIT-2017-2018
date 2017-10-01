bddd0f451065aefb1fc74c52f7b356bfa0450184
Josef Kolář <thejoeejoee@gmail.com>
2017-10-02 00:35:52 +0200

    Change instruction operand data to named structure

--- src/instruction.h
+++ src/instruction.h
@@ -103,18 +103,20 @@ typedef enum type_instruction_operand_t {
     TYPE_INSTRUCTION_OPERAND_LABEL,
 } TypeInstructionOperand;
 
+typedef union code_instruction_operand_data_t {
+    int integer;
+    bool boolean;
+    double floating_point;
+    char* string;
+} CodeInstructionOperandData;
+
 typedef struct code_instruction_operand_t {
     TypeInstructionOperand type;
     // variable specific
     char* identifier; // TODO: only identifier name? or ptr to symbol table?
     // constant specific
     short data_type; // TODO: add enum for data types
-    union {
-        int integer;
-        bool boolean;
-        double floating_point;
-        char* string;
-    };
+    CodeInstructionOperandData data;
     // label specific
     char* label; // TODO: only label name? or standalone struct with additional info?
 } CodeInstructionOperand;

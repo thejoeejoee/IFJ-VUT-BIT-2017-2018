295dc8d376c5b3b1a56cfcece272999ecced023d
Josef Kolář <thejoeejoee@gmail.com>
2017-10-02 14:45:50 +0200

    Dummy code generation

--- src/code_generator.c
+++ src/code_generator.c
@@ -1,6 +1,9 @@
 #include "code_generator.h"
 #include "memory.h"
 
+void generate(CodeGenerator* pGenerator, enum type_instruction_t instruction, CodeInstructionOperand* pOperand,
+              CodeInstructionOperand* pInstruction_operand, CodeInstructionOperand* pInstructionOperand);
+
 CodeGenerator* code_generator_init() {
     CodeGenerator* generator = memory_alloc(sizeof(CodeGenerator));
 
@@ -9,3 +12,29 @@ CodeGenerator* code_generator_init() {
     return generator;
 }
 
+
+void generate_ADD(CodeGenerator* generator,
+                  CodeInstructionOperand* op0,
+                  CodeInstructionOperand* op1,
+                  CodeInstructionOperand* op2) {
+
+    ASSERT(op0->type == TYPE_INSTRUCTION_OPERAND_VARIABLE);
+
+    generate(generator, I_ADD, op0, op1, op2);
+}
+
+void generate_INT2FLOAT(CodeGenerator* generator,
+                        CodeInstructionOperand* op0,
+                        CodeInstructionOperand* op1) {
+
+    ASSERT(op0->type == TYPE_INSTRUCTION_OPERAND_VARIABLE);
+
+    generate(generator, I_ADD, op0, op1, NULL);
+}
+
+void generate(CodeGenerator* pGenerator, enum type_instruction_t instruction, CodeInstructionOperand* pOperand,
+              CodeInstructionOperand* pInstruction_operand, CodeInstructionOperand* pInstructionOperand
+) {
+
+
+}

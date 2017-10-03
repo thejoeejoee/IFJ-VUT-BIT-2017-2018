0d4d47be278d37a0e2375de4875e8b58111931f3
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-10-03 20:18:49 +0200

    Add macro to get overloaded macro with max 4 params

--- src/common.h
+++ src/common.h
@@ -5,5 +5,6 @@
 #define MSVC_EXPAND(x) x
 #define GET_FIRST_ARG(N, ...) N
 #define GET_OVERLOADED_MACRO12(_1, _2, ...) MSVC_EXPAND(GET_FIRST_ARG(__VA_ARGS__, 0))
+#define GET_OVERLOADED_MACRO34(_1, _2, _3, _4, ...) MSVC_EXPAND(GET_FIRST_ARG(__VA_ARGS__, 0))
 
 #endif // _COMMON_H

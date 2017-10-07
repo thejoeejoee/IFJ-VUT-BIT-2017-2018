5370ea86c3c1094bfa7cc37e30d119e4f6686239
Josef Kolář <thejoeejoee@gmail.com>
2017-10-07 14:35:38 +0200

    Add test for instruction operand type validation

--- test/code_generator.cpp
+++ test/code_generator.cpp
@@ -72,11 +72,48 @@ TEST_F(CodeGeneratorTestFixture, OneInstructionWithOperands) {
 }
 
 
+TEST_F(CodeGeneratorTestFixture, OneInstructionWithMultipleOperands) {
+    CodeInstructionOperand* label_operand = code_instruction_operand_init_label("test");
+    CodeInstructionOperand* label_operand2 = code_instruction_operand_init_label("test");
+    CodeInstructionOperand* int_operand0 = code_instruction_operand_init_integer(145);
+    CodeInstructionOperand* int_operand1 = code_instruction_operand_init_integer(141875);
+    CodeInstructionOperand* int_operand2 = code_instruction_operand_init_integer(863);
+    CodeInstructionOperand* int_operand3 = code_instruction_operand_init_integer(719);
+
+    EXPECT_TRUE(code_generate_I_JUMP_IF_EQUAL(code_generator, label_operand, int_operand0, int_operand1));
+    EXPECT_TRUE(code_generate_I_JUMP_IF_EQUAL(code_generator, label_operand2, int_operand2, int_operand3));
+
+    EXPECT_NE(
+            code_generator->first,
+            code_generator->last
+    );
+    EXPECT_EQ(
+            code_generator->first->op2,
+            int_operand1
+    );
+    EXPECT_EQ(
+            code_generator->last->op2,
+            int_operand3
+    );
+}
+
+
 TEST_F(CodeGeneratorTestFixture, InvalidOperandType) {
     CodeInstructionOperand* label_operand = code_instruction_operand_init_label("test");
+    CodeInstructionOperand* int_operand = code_instruction_operand_init_integer(42);
     DISABLE_LOG(
             EXPECT_FALSE(
                     code_generate_I_WRITE(code_generator, label_operand)
+            ) << "Invalid operand type.";
+    );
+    DISABLE_LOG(
+            EXPECT_FALSE(
+                    code_generate_I_JUMP_IF_EQUAL(code_generator, label_operand, label_operand, label_operand)
+            ) << "Invalid operand type.";
+    );
+    DISABLE_LOG(
+            EXPECT_FALSE(
+                    code_generate_I_JUMP_IF_EQUAL(code_generator, label_operand, int_operand, label_operand)
             ) << "Invalid operand type.";
     );
     EXPECT_EQ(
@@ -87,4 +124,5 @@ TEST_F(CodeGeneratorTestFixture, InvalidOperandType) {
             code_generator->last,
             nullptr
     );
+    code_instruction_operand_free(&int_operand);
 }

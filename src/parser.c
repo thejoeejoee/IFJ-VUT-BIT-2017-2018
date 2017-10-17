5b9a3a5457b10e1aae56b0a55d931a3324d58af8
Martin Kobelka <martinkobelka@gmail.com>
2017-10-17 19:54:20 +0200

    Repair semantic error

--- src/parser.c
+++ src/parser.c
@@ -647,6 +647,7 @@ bool parser_parse_input(Parser* parser) {
                                                     token.data
                                             );
                                             if(NULL == symbol_variable) {
+                                        parser->parser_semantic->error_report.error_code = 3;
                                         token_free(&token);
                                         return false;
                                     }

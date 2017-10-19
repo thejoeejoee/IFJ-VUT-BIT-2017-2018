1d754086383976cb54c26b291d965973edb6f1b6
Martin Kobelka <martinkobelka@gmail.com>
2017-10-19 23:00:02 +0200

    Repair failing test

--- test/parser.cpp
+++ test/parser.cpp
@@ -856,8 +856,8 @@ scope
 	print len;
 
 	dim str as string
-	string = !"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	len = strlen(string)
+	cokolivjennestring = !"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	len = strlen(cokolivjennestring)
 	print len;
 
 end scope

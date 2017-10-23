f834a63b00c965718b4a66c9fe4cec595da1af26
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-10-24 00:04:57 +0200

    Fix remove always true condition check

--- src/dynamic_string.c
+++ src/dynamic_string.c
@@ -5,7 +5,6 @@
 
 
 String* string_init_with_capacity(size_t initial_capacity) {
-    ASSERT(initial_capacity >= 0);
     String* string = (String*) memory_alloc(sizeof(String));
     string->content = (char*) memory_alloc(sizeof(char) * initial_capacity + 1);
     string->content[0] = 0; //c-string terminator

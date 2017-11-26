30133402e6e5e0c71c70136ea2b08bd5a647ecbd
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-11-26 01:30:16 +0100

    Finish oriented graph

--- src/oriented_graph.c
+++ src/oriented_graph.c
@@ -34,7 +34,10 @@ GraphNodeBase* oriented_graph_new_node(OrientedGraph* graph)
     if(graph->nodes_count >= graph->capacity) {
         GraphNodeBase** nodes = graph->nodes;
         graph->nodes = memory_alloc(sizeof(GraphNodeBase*) * graph->capacity * 2);
-        memmove(graph->nodes, nodes, sizeof(GraphNodeBase*) * graph->capacity);
+
+        for(unsigned int i = 0; i < graph->capacity; i++)
+            graph->nodes[i] = nodes[i];
+        memory_free(nodes);
 
         for(unsigned int i = graph->capacity; i < graph->capacity * 2; i++)
             graph->nodes[i] = NULL;
@@ -53,6 +56,7 @@ GraphNodeBase* oriented_graph_new_node(OrientedGraph* graph)
 
             graph->nodes_count++;
             graph->nodes[i] = new_node;
+            break;
         }
     }
 
@@ -64,20 +68,35 @@ GraphNodeBase* oriented_graph_new_node(OrientedGraph* graph)
 
 void oriented_graph_remove_node(OrientedGraph* graph, unsigned int node_id)
 {
+    NULL_POINTER_CHECK(graph, );
+
+    GraphNodeBase* node = oriented_graph_node(graph, node_id);
+    if(node == NULL)
+        return;
+
+    graph->nodes[node_id] = NULL;
+    set_int_free(&(node->out_edges));
+    memory_free(node);
 
     graph->nodes_count--;
 }
 
 GraphNodeBase* oriented_graph_node(OrientedGraph* graph, unsigned int node_id)
 {
-//    for(unsigned in)
-    return NULL;
+    NULL_POINTER_CHECK(graph, NULL);
+
+    if(node_id >= graph->capacity)
+        return NULL;
+
+    return graph->nodes[node_id];
 }
 
 void oriented_graph_clear(OrientedGraph* graph)
 {
     for(unsigned int i = 0; i < graph->capacity; i++) {
         if(graph->nodes[i] != NULL) {
+            // TODO add free data callback
+            set_int_free(&(graph->nodes[i]->out_edges));
             memory_free(graph->nodes[i]);
             graph->nodes[i] = NULL;
         }
@@ -85,4 +104,41 @@ void oriented_graph_clear(OrientedGraph* graph)
 
     graph->nodes_count = 0;
     graph->capacity = 0;
+}
+
+void oriented_graph_print(OrientedGraph* graph)
+{
+    printf("OrientedGraph(\n");
+    for(unsigned int i = 0; i < graph->capacity; i++) {
+        if(graph->nodes[i] == NULL)
+            continue;
+
+        const GraphNodeBase* node = graph->nodes[i];
+        printf("Node %d -> ", node->id);
+        set_int_print(node->out_edges);
+        printf("\n");
+    }
+
+    printf(")\n");
+}
+
+void oriented_graph_connect_nodes(OrientedGraph* graph, GraphNodeBase* from, GraphNodeBase* to)
+{
+    NULL_POINTER_CHECK(graph, );
+    NULL_POINTER_CHECK(from, );
+    NULL_POINTER_CHECK(to, );
+
+    set_int_add(from->out_edges, to->id);
+}
+
+void oriented_graph_connect_nodes_by_ids(OrientedGraph* graph, unsigned int from, unsigned int to)
+{
+    NULL_POINTER_CHECK(graph, );
+
+    GraphNodeBase* from_node = oriented_graph_node(graph, from);
+    GraphNodeBase* to_node = oriented_graph_node(graph, to);
+    if(from_node == NULL || to_node == NULL)
+        return;
+
+    oriented_graph_connect_nodes(graph, from_node, to_node);
 }
--- src/oriented_graph.h
+++ src/oriented_graph.h
@@ -23,6 +23,12 @@ GraphNodeBase* oriented_graph_new_node(OrientedGraph* graph);
 void oriented_graph_remove_node(OrientedGraph* graph, unsigned int node_id);
 
 GraphNodeBase* oriented_graph_node(OrientedGraph* graph, unsigned int node_id);
+void oriented_graph_connect_nodes(OrientedGraph* graph, GraphNodeBase* from, GraphNodeBase* to);
+
+void oriented_graph_connect_nodes_by_ids(OrientedGraph* graph, unsigned int from, unsigned int to);
+
 void oriented_graph_clear(OrientedGraph* graph);
+
+void oriented_graph_print(OrientedGraph* graph);
 
 #endif // ORIENTEDGRAPH_H

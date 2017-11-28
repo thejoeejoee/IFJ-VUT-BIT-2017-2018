1271638a85edf7ab0d360f80ee40d9b17b5eed42
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-11-28 01:31:27 +0100

    Implement CodeBlock (node)

--- src/meta_data_code_block.c
+++ src/meta_data_code_block.c
@@ -1,3 +1,54 @@
 #include "meta_data_code_block.h"
 
-int a = 4;
+void init_code_block(GraphNodeBase* base)
+{
+    NULL_POINTER_CHECK(base, );
+
+    CodeBlock* v = (CodeBlock*)base;
+    v->direct_jump = false;
+    v->instructions_count = 0;
+    v->instructions = NULL;
+    v->last_instruction = NULL;
+    v->conditional_jump = set_int_init();
+}
+
+void free_code_block(GraphNodeBase* base)
+{
+    NULL_POINTER_CHECK(base, );
+
+    CodeBlock* v = (CodeBlock*)base;
+    set_int_free(&v->conditional_jump);
+}
+
+void code_block_set_start_instruction(CodeBlock* block, CodeInstruction* instruction)
+{
+    NULL_POINTER_CHECK(block, );
+    NULL_POINTER_CHECK(instruction, );
+
+    block->instructions = instruction;
+    block->last_instruction = instruction;
+    block->instructions_count++;
+}
+
+void code_block_add_next_instruction(CodeBlock* block)
+{
+    NULL_POINTER_CHECK(block, );
+    if(block->instructions_count == 0) {
+        LOG_WARNING("Bad adding instruction to code block");
+        return;
+    }
+
+    block->instructions_count++;
+    block->last_instruction = block->last_instruction->next;
+}
+
+void code_block_add_instruction(CodeBlock* block, CodeInstruction* instruction)
+{
+    NULL_POINTER_CHECK(block, );
+    NULL_POINTER_CHECK(instruction, );
+
+    if(block->instructions_count == 0)
+        code_block_set_start_instruction(block, instruction);
+    else
+        code_block_add_next_instruction(block);
+}
--- src/meta_data_code_block.h
+++ src/meta_data_code_block.h
@@ -2,5 +2,23 @@
 #define META_DATA_CODE_BLOCK_H
 
 #include "oriented_graph.h"
+#include "code_instruction.h"
+
+typedef struct {
+    GraphNodeBase base;
+    bool direct_jump;
+    size_t instructions_count;
+    CodeInstruction* instructions;
+    CodeInstruction* last_instruction;
+    SetInt* conditional_jump;
+} CodeBlock;
+
+void init_code_block(GraphNodeBase* base);
+void free_code_block(GraphNodeBase* base);
+void code_block_set_start_instruction(CodeBlock* block, CodeInstruction* instruction);
+void code_block_add_next_instruction(CodeBlock* block);
+
+// INFO need to instrctuion which are consecutively
+void code_block_add_instruction(CodeBlock* block, CodeInstruction* instruction);
 
 #endif // META_DATA_CODE_BLOCK_H
